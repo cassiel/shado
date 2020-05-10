0829da9cd6d919a2aa37aec66245d3ef38d9fe44
Nick (Git client) <nick@cassiel.com>
2020-05-10 17:51:57 +0100

    Modified calling for new() - in demo files.

--- shado.lua
+++ shado.lua
@@ -5,11 +5,11 @@ local types = require "shado.lib.types"
 local blocks = require "shado.lib.blocks"
 local renderers = require "shado.lib.renderers"
 
-local block = blocks.Block.new(4, 4):fill(types.LampState.ON)
+local block = blocks.Block:new(4, 4):fill(types.LampState.ON)
 
 local g = grid.connect()
 
-renderers.VariableBlockRenderer.new(16, 8, g):render(block)
+renderers.VariableBlockRenderer:new(16, 8, g):render(block)
 
 --g:all(7)
 --g:refresh()
--- speed-shado.lua
+++ speed-shado.lua
@@ -12,27 +12,27 @@ local blocks = require "shado.lib.blocks"
 local frames = require "shado.lib.frames"
 local renderers = require "shado.lib.renderers"
 
-local block = blocks.Block.new("1")
+local block = blocks.Block:new("1")
 
 local g = grid.connect()
-local renderer = renderers.VariableBlockRenderer.new(16, 8, g)
+local renderer = renderers.VariableBlockRenderer:new(16, 8, g)
 
 --[[ EFFORT 1
 for x = 1, 16 do
     for y = 1, 8 do
-        local frame = frames.Frame.new():add(block, x, y)
-        renderer:render(frame)        
+        local frame = frames.Frame:new():add(block, x, y)
+        renderer:render(frame)
     end
 end
 ]]
 
 -- EFFORT 2
-local frame = frames.Frame.new():add(block, 1, 1)
+local frame = frames.Frame:new():add(block, 1, 1)
 print("get", frame.get)
 print("moveTo", frame.moveTo)
 for x = 1, 16 do
     for y = 1, 8 do
         frame:moveTo(block, x, y)
-        renderer:render(frame)        
+        renderer:render(frame)
     end
 end
