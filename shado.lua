43669398107b103852cb12292c2055e869599ba5
Nick (Git client) <nick@cassiel.com>
2021-12-30 18:23:52 +0100

    Tweaks.

--- shado.lua
+++ shado.lua
@@ -3,7 +3,9 @@
 --
 -- shado demonstrator script.
 -- Nick Rothwell, nick@cassiel.com.
---
+-- https://llllllll.co/t/51408
+
+
 -- Source: [https://github.com/cassiel/shado](https://github.com/cassiel/shado).
 --
 -- Introduction and description [here](https://github.com/cassiel/shado/blob/master/README.MANUAL.org).
@@ -62,12 +64,12 @@ local appDisplayText = ""
 function redraw()
     screen.clear()
 
-    -- Build table of lines of text:    
+    -- Build table of lines of text:
     local tab = { }
     for line in appDisplayText:gmatch("%s*([^\n]+)") do
         table.insert(tab, line)
     end
-    
+
     showAppIndex()
 
     -- Display:
@@ -81,7 +83,7 @@ function redraw()
             screen.font_face(1)     -- Default.
             screen.font_size(8)
         end
-    
+
         screen.move(0, i * 10 + 5)
         screen.text(v)
     end
@@ -105,7 +107,7 @@ local function scroller(oldFrom, oldTo, oldStep, newOffset, displayText)
         -- We've already brought the new app to the top (index 1):
         local oldAppLayer = frame:get(2)
         local newAppLayer = frame:get(1)
-    
+
         -- Scroll the pair of app layers up or down:
         for i = oldFrom, oldTo, oldStep do
             frame:moveTo(oldAppLayer, 1, i)
@@ -113,7 +115,7 @@ local function scroller(oldFrom, oldTo, oldStep, newOffset, displayText)
             renderer:render(frame)
             clock.sleep(0.02)
         end
-        
+
         appDisplayText = displayText
         redraw()
         keysBlocked = false
@@ -126,16 +128,16 @@ local function selectApp(app, sense)
         TODO: we should really wrap each app in a mask to avoid it getting any
         out-of-bounds presses (or displaying out-of-bounds) when it's pushed away.
     ]]
-    
+
     -- Immediately clear screen (we'll draw the new text on end of scroll):
     appDisplayText = ""
     redraw()
-    
+
     local displayText = app.displayText or ""
-    
+
     -- New application to top of display stack:
     frame:top(app.layer)
-    
+
     if sense == 1 then
         -- Old app moves up, corner from (1, 0) to (1, -7).
         -- New app is below, offset 8:
@@ -170,7 +172,7 @@ function key(n, z)
                 newIndex = currentAppIndex
                 sense = 0
             end
-        
+
             currentAppIndex = newIndex
             currentApp = apps[currentAppIndex]
             selectApp(currentApp, sense)
@@ -188,7 +190,7 @@ end
 
 function init()
     renderer:render(frame)
-    
+
     local mgr = manager.PressManager:new(frame)
 
     g.key = function (x, y, how)
@@ -202,6 +204,6 @@ function init()
     counter.count = -1
     counter.event = service
     counter:start()
-    
+
     selectApp(currentApp, 0)
 end
